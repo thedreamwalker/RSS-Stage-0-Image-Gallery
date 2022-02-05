f1f9f8bf5d43b9754d583995413e5a3d2eededba
Veronica <vhayami@gmail.com>
2022-02-05 23:02:29 +0300

    feat: add change volume and shuffle tracks

--- audio-player/index.js
+++ audio-player/index.js
@@ -1,16 +1,20 @@
 const audioBtn = document.querySelector('.control-switcher');
 const nextBtn = document.querySelector('.control-next');
 const prevBtn = document.querySelector('.control-previous');
+const shuffleBtn = document.querySelector('.control-shuffle');
 
 const textAuthor = document.querySelector('.track-author');
 const textName = document.querySelector('.track-name');
+const backgroundImage = document.querySelector('.background-img');
+const trackImage = document.querySelector('.track-img');
 
 let progress = document.querySelector('.player-progress-bar');
-//let currentTime = document.querySelector('.player-current-time');
+let currentTime = document.querySelector('.player-current-time');
 
 //const audio = new Audio('./assets/audio/beyonce.mp3');
 const audio = new Audio();
 let isPlay = false;
+let isShuffle = false;
 let playNum = 0;
 
 // const tracks = ['./assets/audio/beyonce.mp3', './assets/audio/dontstartnow.mp3', './assets/audio/steffanargus.mp3'];
@@ -19,23 +23,26 @@ const tracks = [
     name: `Don't Hurt Yourself`,
     author: `Beyonce`,
     src: './assets/audio/beyonce.mp3',
-    duration: '3:53'
+    duration: '3:53',
+    img: './assets/img/lemonade.png'
   },
   {
     name: `Don't Start Now`,
     author: `Dua Lipa`,
     src: './assets/audio/dontstartnow.mp3',
-    duration: '3:23'
+    duration: '3:23',
+    img: './assets/img/dontstartnow.png'
   },
   {
     name: `Ship in a Bottle`,
     author: `Steffan Argus`,
     src: './assets/audio/steffanargus.mp3',
-    duration: '5:14'
+    duration: '5:14',
+    img: './assets/img/shipinabottle.jpg'
   }
 ];
 
-/*
+
 // начальные параметры
 audio.addEventListener(
   "loadeddata",
@@ -48,6 +55,8 @@ audio.addEventListener(
   false
 );
 
+audio.src = tracks[playNum].src;
+
 //click on timeline to skip around
 
 const timeline = document.querySelector(".player-progress");
@@ -57,11 +66,11 @@ timeline.addEventListener("click", e => {
   const timeToSeek = e.offsetX / parseInt(timelineWidth) * audio.duration;
   audio.currentTime = timeToSeek;
   
-}, false);*/
+}, false);
 
 // запускает плеер и останавливает
 function playAudio() {
-audio.src = tracks[playNum].src;
+//audio.src = tracks[playNum].src;
 textAuthor.innerHTML = tracks[playNum].author;
 textName.innerHTML = tracks[playNum].name;
 
@@ -76,12 +85,18 @@ textName.innerHTML = tracks[playNum].name;
     audio.pause();
   }
 }
-/*
+
 // следующий трек
 const playNext = x => {
   console.log('Нам пришло ' + playNum);
   playNum += 1;
   playNum > tracks.length - 1 ? playNum = 0 : playNum = playNum;
+
+  if (isShuffle) {
+    playNum = randomTrack(0, tracks.length);
+  }
+
+  console.log(tracks.length);
 
   if (!isPlay) {
     audioBtn.classList.add('control-switcher-pause');
@@ -90,7 +105,11 @@ const playNext = x => {
   isPlay = true;
   textAuthor.innerHTML = tracks[playNum].author;
   textName.innerHTML = tracks[playNum].name;
+  backgroundImage.src = tracks[playNum].img;
+  trackImage.src = tracks[playNum].img;
   audio.play();
+
+  console.log('Текущее значение ' + playNum);
 }
 
 // предыдущий трек
@@ -107,9 +126,11 @@ const playPrev = x => {
   isPlay = true;
   textAuthor.innerHTML = tracks[playNum].author;
   textName.innerHTML = tracks[playNum].name;
+  backgroundImage.src = tracks[playNum].img;
+  trackImage.src = tracks[playNum].img;
   audio.play();
-}*/
-/*
+}
+
 // прогресс воспроизведения
 
 const updateProgress = x => {
@@ -125,12 +146,38 @@ const formatTime = time => {
   var sec = Math.floor(time % 60);
   return min + ':' + ((sec<10) ? ('0' + sec) : sec);
 }
-*/
+
+// регулировка звука
+
+const volumeSlider = document.querySelector(".volume-progress");
+volumeSlider.addEventListener('click', e => {
+  const sliderHeight = window.getComputedStyle(volumeSlider).height;
+  console.log('значение оффсета ' + e.offsetY);
+  const newVolume = e.offsetY / parseInt(sliderHeight);
+  audio.volume = 1 - newVolume;
+  document.querySelector(".volume-progress-bar").style.height = newVolume * 100 + '%';
+}, false)
+
+
+shuffleBtn.addEventListener('click', e => {
+  shuffleBtn.classList.toggle('is-active');
+  if (!isShuffle) {
+    isShuffle = true;
+  } else {
+    isShuffle = false;
+  }
+}, false)
+
+function randomTrack(min, max){
+  const r = Math.random()*(max-min) + min;
+  return Math.floor(r);
+}
+
 
 audioBtn.addEventListener('click', playAudio);
 nextBtn.addEventListener('click', playNext);
 prevBtn.addEventListener('click', playPrev);
-//audio.addEventListener('timeupdate', updateProgress);
+audio.addEventListener('timeupdate', updateProgress);
 
 
 
@@ -425,4 +472,3 @@ function getTimeCodeFromNum(num) {
 }
 
 */
-
