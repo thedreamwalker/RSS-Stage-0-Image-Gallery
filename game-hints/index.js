0c902594f7a1d185d245ee460a478e6e1bdcc14c
Veronica <vhayami@gmail.com>
2022-02-20 17:41:51 +0300

    feat: add choose size fied, add style

--- game-hints/index.js
+++ game-hints/index.js
@@ -1,7 +1,9 @@
 const memoryField = document.querySelector('.memory-field');
+const resultField = document.querySelector('.memory-result');
+const buttonStart = document.querySelector('.start-game');
 let numberMove = 0;
 let checkedPair = 0;
-const sizeField = 12;
+let sizeField = 12;
 let isFlipped = false;
 let lockBoard = false;
 let firstCard, secondCard;
@@ -9,73 +11,112 @@ let scores = [];
 
 const allCards = [
   {
-    name: 'react',
-    src: './assets/svg/react.svg'
+    name: 'akira',
+    src: './assets/png/akira.png'
   },
   {
-    name: 'react',
-    src: './assets/svg/react.svg'
+    name: 'akira',
+    src: './assets/png/akira.png'
   },
   {
-    name: 'angular',
-    src: './assets/svg/angular.svg'
+    name: 'ryuuji',
+    src: './assets/png/ryuuji.png'
   },
   {
-    name: 'angular',
-    src: './assets/svg/angular.svg'
+    name: 'ryuuji',
+    src: './assets/png/ryuuji.png'
   },
   {
-    name: 'aurelia',
-    src: './assets/svg/aurelia.svg'
+    name: 'morgana',
+    src: './assets/png/morgana.png'
   },
   {
-    name: 'aurelia',
-    src: './assets/svg/aurelia.svg'
+    name: 'morgana',
+    src: './assets/png/morgana.png'
   },
   {
-    name: 'backbone',
-    src: './assets/svg/backbone.svg'
+    name: 'ann',
+    src: './assets/png/ann.png'
   },
   {
-    name: 'backbone',
-    src: './assets/svg/backbone.svg'
+    name: 'ann',
+    src: './assets/png/ann.png'
   },
   {
-    name: 'ember',
-    src: './assets/svg/ember.svg'
+    name: 'yuusuke',
+    src: './assets/png/yuusuke.png'
   },
   {
-    name: 'ember',
-    src: './assets/svg/ember.svg'
+    name: 'yuusuke',
+    src: './assets/png/yuusuke.png'
   },
   {
-    name: 'vue',
-    src: './assets/svg/vue.svg'
+    name: 'makoto',
+    src: './assets/png/makoto.png'
   },
   {
-    name: 'vue',
-    src: './assets/svg/vue.svg'
+    name: 'makoto',
+    src: './assets/png/makoto.png'
+  },
+  {
+    name: 'joker',
+    src: './assets/png/joker.png'
+  },
+  {
+    name: 'joker',
+    src: './assets/png/joker.png'
+  },
+  {
+    name: 'haru',
+    src: './assets/png/haru.png'
+  },
+  {
+    name: 'haru',
+    src: './assets/png/haru.png'
+  },
+  {
+    name: 'futaba',
+    src: './assets/png/futaba.png'
+  },
+  {
+    name: 'futaba',
+    src: './assets/png/futaba.png'
+  },
+  {
+    name: 'akechi',
+    src: './assets/png/akechi.png'
+  },
+  {
+    name: 'akechi',
+    src: './assets/png/akechi.png'
   }
 ];
 
 
 const cardsSet = num => {
+  if (Number(num) === 20) {
+    memoryField.classList.add('big');
+  }
   for (let i = 0; i < num; i++) {
     const div = document.createElement('div');
     div.classList.add('memory-card')
     const cardFront = document.createElement('img');
-    cardFront.classList.add('front-face')
+    cardFront.classList.add('front-face');
     cardFront.src = allCards[i].src;
     div.setAttribute("data-name", allCards[i].name);
     const cardBack = document.createElement('img');
     cardBack.classList.add('back-face')
-    cardBack.src = "./assets/svg/js-badge.svg";
+    cardBack.src = "./assets/png/back-face.png";
     memoryField.append(div);
     div.append(cardFront);
     div.append(cardBack);
   }
 
 };
+
+if (localStorage.getItem('field')) {
+  sizeField = localStorage.getItem('field');  
+}
 
 cardsSet(sizeField);
 
@@ -105,7 +146,8 @@ function checkForMatch() {
      disableCards();
      checkedPair += 1;
      if (checkedPair === sizeField / 2) {
-      setTimeout("alert('Ура, победа!');", 1000);
+      //setTimeout("alert('Ура, победа!');", 1000);
+      resultField.classList.add('on');
       scores.push(numberMove);
       localStorage.setItem( 'score', JSON.stringify(scores));
     }
@@ -158,8 +200,7 @@ const tableSet = num => {
   for (let i = 0; i < scores.length; i++) {
     const div = document.createElement('div');
     div.classList.add('memory-result-table-item');
-    console.log(scores);
-    div.innerHTML = `На игру было затрачено ${scores[i]} ходов`;
+    div.innerHTML = `It had taken ${scores[i]} moves`;
     document.querySelector('.memory-result-table').append(div);
   }
 };
@@ -169,8 +210,7 @@ tableSet();
 function getLocalStorage() {
   if (localStorage.getItem('score')) {
       const result = localStorage.getItem('score');
-      scores = JSON.parse(result);
-      
+      scores = JSON.parse(result);   
   }
 }  
 
@@ -180,4 +220,15 @@ function setLocalStorage() {
 
 window.addEventListener('load', getLocalStorage)  
 window.addEventListener('beforeunload', setLocalStorage);
+
+function changeFiled() {
+  const element = document.getElementsByName('startgame');
+  for (let i = 0; i < element.length; i++) {
+    element[0].checked ? sizeField = 12 : sizeField = 20;
+  }
+  localStorage.setItem('field', sizeField);
+  location.reload()
+}
+
+buttonStart.addEventListener('click', changeFiled);
 
